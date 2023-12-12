e297f933bb0ac73b5eb0498dfda2c1546467588f
Esperanza <esperanza.llontop27@gmail.com>
2023-12-12 21:28:16 +0100

    replce services with async and await

---
+++ pages/error/error.js
@@ -0,0 +1,10 @@
+/* const params = new URLSearchParams(window.location.search);
+const errMsg = params.get("err");
+
+const getTitle = () => {
+  document.getElementById("error").innerText = errMsg;
+};
+
+window.onload = () => {
+  getTitle();
+}; */
--- services/services.js
+++ services/services.js
@@ -1,60 +1,90 @@
-const token ="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc1Y2ExZDNkYWRhMDAwMThhNjllNzQiLCJpYXQiOjE3MDIyMTgyNjksImV4cCI6MTcwMzQyNzg2OX0.tSROMA9zIya85k0CXYLA0gQVb8cvRt31uMWya6cuvzA";
+const token =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc1Y2ExZDNkYWRhMDAwMThhNjllNzQiLCJpYXQiOjE3MDIyMTgyNjksImV4cCI6MTcwMzQyNzg2OX0.tSROMA9zIya85k0CXYLA0gQVb8cvRt31uMWya6cuvzA";
 const url = "https://striveschool-api.herokuapp.com/api/product/";
 const headers = {
-  "Authorization": token,
+  Authorization: token,
   "Content-Type": "application/json",
 };
 
 /* add GET ALL */
-const loadProducts = () => {
-  fetch(url, {
+const loadProducts = async () => {
+  const resp = await fetch(url, {
     headers,
     method: "GET",
-  })
-    .then((resp) => resp.json())
-    .then((resp) => displayProducts(resp))
-    .catch((err) => console.error(err));
+  });
+  if (resp.ok) {
+    const data = await resp.json();
+    displayProducts(data);
+  } else {
+    /* window.location.href = `../pages/error/error.html`; */
+    window.alert("Products not found");
+  }
 };
 
 /* add GET BY ID (params: id)*/
-const loadProductById = (id, mode) => {
-  fetch("https://striveschool-api.herokuapp.com/api/product/"+ id, {
-    headers,
-    method: "GET",
-  })
-    .then((resp) => resp.json())
-    .then((resp) => mode === 'Edit' ? editProducts(resp) : detailsProduct(resp))
-    .catch((err) => console.error(err));
+const loadProductById = async (id, mode) => {
+  const resp = await fetch(
+    "https://striveschool-api.herokuapp.com/api/product/" + id,
+    {
+      headers,
+      method: "GET",
+    }
+  );
+
+  if (resp.ok) {
+    const data = await resp.json();
+    mode === "Edit" ? editProducts(data) : detailsProduct(data);
+  } else {
+    /* window.location.href = `../pages/error/error.html`; */
+    window.alert("Product not found");
+  }
 };
 
 /* add PUT (params: id, data) */
-const updateProduct = (id, payload) => {
-  fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`, {
-    headers,
-    method: "PUT",
-    body: JSON.stringify(payload),
-  })
-    .then((resp) => resp.json())
-    .catch((err) => console.error(err));
+const updateProduct = async (id, payload) => {
+  const resp = await fetch(
+    `https://striveschool-api.herokuapp.com/api/product/${id}`,
+    {
+      headers,
+      method: "PUT",
+      body: JSON.stringify(payload),
+    }
+  );
+
+  if (!resp.ok) {
+    /* window.location.href = `../pages/error/error.html`; */
+    window.alert("Update with error");
+  }
 };
 
 /* add POST (params: data) */
-const createProduct = (payload) => {
-  fetch(`https://striveschool-api.herokuapp.com/api/product`, {
-    headers,
-    method: "POST",
-    body: JSON.stringify(payload),
-  })
-    .then((resp) => resp.json())
-    .catch((err) => console.error(err));
+const createProduct = async (payload) => {
+  const resp = await fetch(
+    `https://striveschool-api.herokuapp.com/api/product`,
+    {
+      headers,
+      method: "POST",
+      body: JSON.stringify(payload),
+    }
+  );
+
+  if (!resp.ok) {
+    /* window.location.href = `../pages/error/error.html`; */
+    window.alert("Create with error");
+  }
 };
 
 /* add DELETE (params: id)*/
-const deleteProduct = (id) => {
-  fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`, {
-    headers,
-    method: "DELETE",
-  })
-    .then((resp) => resp.json())
-    .catch((err) => console.error(err));
+const deleteProduct = async (id) => {
+  const resp = await fetch(
+    `https://striveschool-api.herokuapp.com/api/product/${id}`,
+    {
+      headers,
+      method: "DELETE",
+    }
+  );
+  if (!resp.ok) {
+    /* window.location.href = `../pages/error/error.html`; */
+    window.alert("Delete with error");
+  }
 };
